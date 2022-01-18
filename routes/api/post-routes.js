7b97b44c86e45408b9d6d7e041a5293a02cef6ea
Joona Lusa <joona.lusa@hotmail.com>
2022-01-18 18:51:56 -0500

    all api end points have been set up

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // Retrieving all the users
 router.get('/', (req, res) => {
@@ -12,9 +12,18 @@ router.get('/', (req, res) => {
                 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
             ],
             include: [{
-                model: User,
-                attributes: ['username']
-            }]
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
         })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -35,9 +44,18 @@ router.get('/:id', (req, res) => {
                 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
             ],
             include: [{
-                model: User,
-                attributes: ['username']
-            }]
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
         })
         .then(dbPostData => {
             if (!dbPostData) {
